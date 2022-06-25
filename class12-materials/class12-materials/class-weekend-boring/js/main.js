73da5ca98cd824b48a3f706ab98917bddf13a58c
siriusscaper <siriusscaper@gmail.com>
2022-06-24 17:54:07 -0700

    changes to folder structure

--- class12-materials/class12-materials/class-weekend-boring/js/main.js
+++ class12-materials/class12-materials/class-weekend-boring/js/main.js
@@ -1,10 +1,17 @@
 document.querySelector('#check').addEventListener('click', check)
 
 function check() {
-
-  const day = document.querySelector('#day').value
-
+  
+  const day = document.querySelector('#day').value.toLowerCase();
+  
   //Conditionals go here
-
-
-}
+  if (day === 'tuesday' || day === 'thursday'){
+    console.log("Class day");
+    document.querySelector('#placeToSee').innerText = 'Class day';
+  } else if (day === 'saturday' || day === 'sunday') {
+    console.log("The weekend")
+    document.querySelector('#placeToSee').innerText = 'The weekend';
+  } else {
+    document.querySelector('#placeToSee').innerText = 'Boring day';
+  }
+};
