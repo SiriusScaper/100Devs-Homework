55ea4c76e926e8496cb77d0e3c31483736b2006e
siriusscaper <siriusscaper@gmail.com>
2022-07-17 19:01:46 -0700

    changes

--- class14-materials/temperature-converter/js/main.js
+++ class14-materials/temperature-converter/js/main.js
@@ -1,1 +1,23 @@
 //Write your pseduo code first! 
+
+document.querySelector('#convertC').addEventListener('click', celsius)
+
+function celsius(){
+  // grab an input and assign it
+  let tempC = document.querySelector('#celsiusToF').value
+  // convert from celsius to fahrenheit
+  tempC = (tempC * 9/5) + 32;
+  //show it in the browser
+  document.querySelector('#outputF').innerText = tempC
+}
+document.querySelector('#convertF').addEventListener('click', fahrenheit)
+
+function fahrenheit(){
+  // grab an input and assign it
+  let tempF = document.querySelector('#fahrenheitToC').value
+  // convert from celsius to fahrenheit
+  tempF = (tempF - 32) * 5/9;
+  //show it in the browser
+  document.querySelector('#outputC').innerText = tempF
+}
+
