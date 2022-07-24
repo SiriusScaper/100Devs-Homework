fb65d1e32eafdf8e903bd919b1035b7a4fa96bad
siriusscaper <siriusscaper@gmail.com>
2022-07-24 14:51:41 -0700

    changes

--- class14-materials/bring-it-home-practice/js/main.js
+++ class14-materials/bring-it-home-practice/js/main.js
@@ -1,14 +1,38 @@
 // *Variables*
 // Create a variable and console log the value
-
+let firstName = 'Rhys'
 // Create a variable, add 10 to it, and alert the value
-
+let age = 2
+age += 10
+alert(age)
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
+function substraction(num1, num2, num3, num4) {
+  let diff = num1 - num2 - num3 - num4;
+  alert(diff)
+}
 
+substraction(16, 10, 4, 2)
 // Create a function that divides one number by another and returns the remainder
+function remainder(num1, num2){
+  return num1 % num2
+}
 
-// *Conditionals*
+console.log(remainder(9, 8));
+// Conditionals
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
-
+function jumanji(num1, num2) {
+  let sum = num1 + num2
+  if (sum > 50) {
+    alert('Jumanji')
+  }  
+}
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
+function multiplication(num1, num2, num3){
+  let product = num1 * num2 * num3
+  if (product % 3 === 0){
+    alert('ZEBRA')
+  }
+}
+
+console.log(multiplication(3, 3, 3));
--- class14-materials/simple-functions/js/main.js
+++ class14-materials/simple-functions/js/main.js
@@ -2,11 +2,21 @@
 const add = (num1, num2) =>{
   alert(num1 + num2)
 }
+
+add(4, 4)
 //create a function that multiplys three numbers and console logs the product
 const multiply = (num1, num2, num3) => {
-  console.log(num1 * num2 * num3);
+  // console.log(num1 * num2 * num3);
+  const product = num1 * num2 * num3
+  console.log(product);
 }
+
+multiply(2, 2, 2)
 //create a function that divides two numbers and returns the ???
 const divide = (num1, num2) => {
-  return num1 / num2
+  // return num1 / num2
+  const quotient = num1 / num2
+  return quotient
 }
+
+divide(16, 2)
