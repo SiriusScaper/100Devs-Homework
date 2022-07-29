ef20306da9b33e4e561d6d051399c337ab6b2eaa
siriusscaper <siriusscaper@gmail.com>
2022-07-29 12:57:44 -0700

    changes

--- class42-materials/todo-list-express/public/js/main.js
+++ class42-materials/todo-list-express/public/js/main.js
@@ -1,21 +1,21 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+const deleteBtn = document.querySelectorAll('.fa-trash') // Creates and assigns a variable of deleteBtn to any elements that have a class of .fa-trash
+const item = document.querySelectorAll('.item span') // Creates and assigns a variable of item to a selection of span tags inside of a parent that has a class of "item"
+const itemCompleted = document.querySelectorAll('.item span.completed') // Create and assigns a varibale of itemCompleted to a selection of spans with a class of "completed" inside a parent with a class of "item"
 
-Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
-})
+Array.from(deleteBtn).forEach((element)=>{ //Creates a copy of the objects stored in deleteBtn as an array and uses the array method forEach to loop through them 
+    element.addEventListener('click', deleteItem) // As the forEach loops through the array it adds an event listener with a property of click to the current element and calls the function deleteItem
+}) //closes the loop
 
-Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
-})
+Array.from(item).forEach((element)=>{ // Creates a copy of the objects stored in item variable as an array and uses the array method forEach to loop through them
+    element.addEventListener('click', markComplete) // As the forEach loops through the array it adds an event listener with a property of click to the current element and calls the function markComplete
+}) //closes the loop
 
-Array.from(itemCompleted).forEach((element)=>{
-    element.addEventListener('click', markUnComplete)
-})
+Array.from(itemCompleted).forEach((element)=>{ // Creates a copy of the objects stored in the itemCompleted variable as an array and uses the array method forEach to loop through them
+    element.addEventListener('click', markUnComplete) // As the forEach loops through the array it adds an event listener with a property of click to the current element and calls the function markUnComplete
+}) // closes the loop
 
-async function deleteItem(){
-    const itemText = this.parentNode.childNodes[1].innerText
+async function deleteItem(){ // Declares an asynchronous function called deleteItem
+    const itemText = this.parentNode.childNodes[1].innerText // Creates and assigns a variable of itemText to the "this" context to select the text of a childNode element based on the relation to the parentNode in the html
     try{
         const response = await fetch('deleteItem', {
             method: 'delete',
--- class42-materials/todo-list-express/server.js
+++ class42-materials/todo-list-express/server.js
@@ -1,52 +1,32 @@
-// import node module for express
-const express = require('express') 
-// set a variable to access express functions
-const app = express() 
-// importing node module for mongodb using the mongoclient
-const MongoClient = require('mongodb').MongoClient 
-// assigning a port number to a variable
-const PORT = 2121 
-// importing an environment file for sensitive config information to access the database
-require('dotenv').config() 
+const express = require('express') // import node module for express
+const app = express() // set a variable to access express functions
+const MongoClient = require('mongodb').MongoClient // importing node module for mongodb using the mongoclient
+const PORT = 2121 // assigning a port number to a variable
+require('dotenv').config() // importing an environment file for sensitive config information to access the database
 
 
-// assigning db as a variable to call mongodb functionality
-let db, 
-// assign a variable with the value of the connection key for mongodb that is stored in the env file
-    dbConnectionStr = process.env.DB_STRING, 
-    // assign a variable to access the specific database for the app
-    dbName = 'todo' 
+let db, // assigning db as a variable to call mongodb functionality
+    dbConnectionStr = process.env.DB_STRING, // assign a variable with the value of the connection key for mongodb that is stored in the env file
+    dbName = 'todo' // assign a variable to access the specific database for the app
 
-// connect and give the key and default value as parameters
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) 
-//after the connection is made do this
-    .then(client => { 
-        // console log that the connection was successful
-        console.log(`Connected to ${dbName} Database`) 
-        // reassign db to the parameter with a parameter of dbname to access the database "todo"
-        db = client.db(dbName) 
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) // connect and give the key and default value as parameters
+    .then(client => { //after the connection is made do this
+        console.log(`Connected to ${dbName} Database`) // console log that the connection was successful
+        db = client.db(dbName) // reassign db to the parameter with a parameter of dbname to access the database "todo"
     })
     
-// use the express template language ejs as the view engine
-app.set('view engine', 'ejs') 
-// set the default static file directory as public
-app.use(express.static('public')) 
-// use urlencoding to simplify route paths using bodyparser
-app.use(express.urlencoded({ extended: true })) 
-// enable parsing of json using bodyparser
-app.use(express.json()) 
+app.set('view engine', 'ejs') // use the express template language ejs as the view engine
+app.use(express.static('public')) // set the default static file directory as public
+app.use(express.urlencoded({ extended: true })) // use urlencoding to simplify route paths using bodyparser
+app.use(express.json()) // enable parsing of json using bodyparser
 
 
-//call an asynchronous read function with req/res params at the root path
-app.get('/',async (request, response)=>{ 
-    // set a variable retrieves the matching collection "todos" and put the result in an array
-    const todoItems = await db.collection('todos').find().toArray() 
-    // set a variable that counts the objects that match the query of not completed
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+app.get('/',async (request, response)=>{ //call an asynchronous read function with req/res params at the root path
+    const todoItems = await db.collection('todos').find().toArray() // set a variable retrieves the matching collection "todos" and put the result in an array
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false}) // set a variable that counts the objects that match the query of not completed
+    response.render('index.ejs', { items: todoItems, left: itemsLeft }) 
     
     // Code that does the same as above, but using promise chaining which is less readable than the async/await syntactical sugar
-
     // db.collection('todos').find().toArray()
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
@@ -58,8 +38,7 @@ app.get('/',async (request, response)=>{
 })
 
 
-// asynchronous function to create a new item and add it to the todos collection
-app.post('/addTodo', (request, response) => {
+app.post('/addTodo', (request, response) => { // asynchronous function to create a new item and add it to the todos collection
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
     .then(result => {
         console.log('Todo Added')
@@ -68,8 +47,7 @@ app.post('/addTodo', (request, response) => {
     .catch(error => console.error(error))
 })
 
-// asynchronous function to update an item as complete in the todos collection in the db
-app.put('/markComplete', (request, response) => {
+app.put('/markComplete', (request, response) => { // asynchronous function to update an item as complete in the todos collection in the db
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
             completed: true
@@ -86,8 +64,7 @@ app.put('/markComplete', (request, response) => {
 
 })
 
-// asynchronous function to update an item as uncompleted in the todos collection in the db
-app.put('/markUnComplete', (request, response) => {
+app.put('/markUnComplete', (request, response) => { // asynchronous function to update an item as uncompleted in the todos collection in the db
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
             completed: false
@@ -104,8 +81,7 @@ app.put('/markUnComplete', (request, response) => {
 
 })
 
-// asynchronous function to delete an item in the todos collection in the db
-app.delete('/deleteItem', (request, response) => {
+app.delete('/deleteItem', (request, response) => { // asynchronous function to delete an item in the todos collection in the db
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})
     .then(result => {
         console.log('Todo Deleted')
@@ -115,8 +91,6 @@ app.delete('/deleteItem', (request, response) => {
 
 })
 
-// start the express server and check for the Port in the env file or default to the variable set
-app.listen(process.env.PORT || PORT, ()=>{
-    // confirm the sever is running and on what port
-    console.log(`Server running on port ${PORT}`)
+app.listen(process.env.PORT || PORT, ()=>{ // start the express server and check for the Port in the env file or default to the variable set
+    console.log(`Server running on port ${PORT}`) // confirm the sever is running and on what port
 })
