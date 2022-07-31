a700054b4618f8481075de600ae8f2aa21b51761
siriusscaper <siriusscaper@gmail.com>
2022-07-31 15:37:37 -0700

    changes

--- class42-materials/todo-list-express/server.js
+++ class42-materials/todo-list-express/server.js
@@ -23,11 +23,11 @@ app.use(express.json()) // enable parsing of JSON content from requests
 
 
 app.get('/',async (request, response)=>{ //call an asynchronous read function with req/res params at the root path
-    const todoItems = await db.collection('todos').find().toArray() // set a variable retrieves the matching collection "todos" and put the result in an array
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false}) // set a variable that counts the objects that match the query of not completed
-    response.render('index.ejs', { items: todoItems, left: itemsLeft }) 
+    const todoItems = await db.collection('todos').find().toArray() // set a variable and awaits the return of all of the items in collection "todos" and puts the result in an array
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false}) // sets a variable and awaits a count of the items that match the query of not completed
+    response.render('index.ejs', { items: todoItems, left: itemsLeft }) // returns the response that renders the ejs file that will show the todoItems and itemsLeft on the page
     
-    // Code that does the same as above, but using promise chaining which is less readable than the async/await syntactical sugar
+    // Code that does the same as above, but using promise chaining which is less readable than the async/await syntactical sugar.
     // db.collection('todos').find().toArray()
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
@@ -35,37 +35,36 @@ app.get('/',async (request, response)=>{ //call an asynchronous read function wi
     //         response.render('index.ejs', { items: data, left: itemsLeft })
     //     })
     // })
-    // .catch(error => console.error(error))
-})
+    .catch(error => console.error(error)) // catches an error
+})// closes the async function
 
 
-app.post('/addTodo', (request, response) => { // asynchronous function to create a new item and add it to the todos collection
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
-    .then(result => {
-        console.log('Todo Added')
-        response.redirect('/')
-    })
-    .catch(error => console.error(error))
-})
+app.post('/addTodo', (request, response) => { // Calls an asynchronous function from the form to create a new item and add it to the todos collection at the '/addTodo' path
+    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false}) // Inserts a new item into the todos collection from the form body and gives it a completed value of false by default
+    .then(result => { // On successful insert do the following
+        console.log('Todo Added') // console log "todo added"
+        response.redirect('/') // Redirect to the root route
+    })// End of the .then promise chain
+    .catch(error => console.error(error)) //If something doesn't work return an error
+})// End of the async function 
 
-app.put('/markComplete', (request, response) => { // asynchronous function to update an item as complete in the todos collection in the db
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-        $set: {
-            completed: true
-          }
+app.put('/markComplete', (request, response) => { // Calls an asynchronous function that updates using the  '/markComplete' route to mark an item as complete in the todos collection in the db
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{ // Looks in the database to match the name of the item passed in from itemFromJS which is the value of itemText
+        $set: { 
+            completed: true // Sets a completed value of true to the item
+        }
     },{
-        sort: {_id: -1},
-        upsert: false
-    })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
-    })
-    .catch(error => console.error(error))
+        sort: {_id: -1}, // Sorts the list after update by descending order
+        upsert: false // Prevents the insert if the item does not already exist
+    })// Ends the update call
+    .then(result => { // On successful insert do the following
+        console.log('Marked Complete') // console log "Marked Complete"
+        response.json('Marked Complete') // Responds with json "Marked Complete" to update the number of todos items left
+    }) // End of the .then promise chain
+    .catch(error => console.error(error)) // If something doesn't work return an error
+}) // End of the async function
 
-})
-
-app.put('/markUnComplete', (request, response) => { // asynchronous function to update an item as uncompleted in the todos collection in the db
+app.put('/markUnComplete', (request, response) => { // Calls an asynchronous function to update an item as uncompleted in the todos collection in the db
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
             completed: false
