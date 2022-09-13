e26564d58063cc48fbef74b4a1ded2fc02051aae
siriusscaper <siriusscaper@gmail.com>
2022-09-13 16:07:20 -0700

    working on class 16 materials

--- class16-materials/age-checker/js/main.js
+++ class16-materials/age-checker/js/main.js
@@ -5,9 +5,29 @@
 //If under 25, tell them they can not rent cars affordably
 //If under 30, tell them they can not rent fancy cars affordably
 //If under over 30, tell them there is nothing left to look forward too
-
+let age = document.querySelector('#danceDanceRevolution').value
+const ageChecker = (age) => {
+  Number(age)
+  let message
+  if (age < 16) {
+    message = "You can't drive"
+  } else if (age >= 16 && age < 18){
+    message = `Don't hate from outside the club, because you can't even get in`
+  } else if (age >= 18 && age < 21) {
+    message = `You can't drink!`
+  } else if (age >= 21 && age < 25) {
+    message = `You can't rent a car affordably`
+  } else if (age >= 25 && age < 30) {
+    message = `You can't rent fancy cars affordably`
+  } else {
+    message = `Nothing left in life to look forward to`
+  }
+  return message
+}
 
 //--- Harder
 //On click of the h1
 //Take the value from the input
 //Place the result of the conditional in the paragraph
+let clickMe = document.querySelector('h1')
+clickMe.addEventListener('click', ageChecker)
--- class16-materials/variable-fun-time/js/main.js
+++ class16-materials/variable-fun-time/js/main.js
@@ -1,18 +1,24 @@
 //--- Easy
 //create a variable and assign it a number
-
+let number = 1
 //minus 10 from that number
-
+number -= 10
 //print that number to the console
-
+console.log(number);
 //--- Medium
 //create a variable that holds a value from the input
-
 //add 25 to that number
-
-//alert that number
-
+// valueOfInput += 25
+// //alert that number
+// alert(valueOfInput)
 //--- Hard
 //create a variable that holds the h1
-
+let clickMe = document.querySelector('h1')
 //add an event listener to that element that console logs the sum of the two previous variables
+
+clickMe.addEventListener('click', sum)
+
+function sum () {
+  let valueOfInput = document.querySelector('#danceDanceRevolution').value
+  console.log(number + Number(valueOfInput))
+}
